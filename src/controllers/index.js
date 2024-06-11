adadfaee9c0586a7c33f424a6e593fd6cbabf6ac
parama <parama.kar@gmail.com>
2024-06-11 17:33:30 +0530

    added health check path

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const { getResponseFormat } = require("../utils");
 const router = express.Router();
 
 router.use("/reviews", require("./reviews"));
 router.use("/stack", require("./stack"));
+router.get("/health", function (req, res) {
+  return getResponseFormat(req, res, 200, {
+    working: true,
+  });
+});
 
 module.exports = router;
